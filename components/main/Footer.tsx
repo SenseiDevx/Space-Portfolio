7e6e3faa91f3ca23a39bedfbf7ed5503f1b54141
Omurbek Kurmanbekov <kurmanbekovomurbek80@gmail.com>
2023-11-11 14:50:44 +0600

    done some bugs

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -14,8 +14,6 @@ const Footer = () => {
         <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] ">
             <div className="w-full flex flex-col items-center justify-center m-auto">
                 <div className="w-full h-full flex flex-row items-center justify-around flex-wrap">
-
-
                     <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
                         <div className="font-bold text-[16px]">Community</div>
                         <p className="flex flex-row items-center my-[15px] cursor-pointer">
@@ -57,14 +55,13 @@ const Footer = () => {
                             <span className="text-[15px] ml-[6px]">Learning about me</span>
                         </p>
                         <p className="flex flex-row items-center my-[15px] cursor-pointer">
-
-                            <span className="text-[15px] ml-[6px]">mifwebchain@gmail.com</span>
+                            <a href="mailto:kurmanbekovomurbek80@gmail.com" className="text-[15px] ml-[6px] ">kurmanbekovomurbek80@gmail.com</a>
                         </p>
                     </div>
                 </div>
 
                 <div className="mb-[20px] text-[15px] text-center">
-                    &copy; WebChain Dev 2023 Inc. All rights reserved
+                    &copy; Made By Omurbek 11.11.2023 Inc.
                 </div>
             </div>
         </div>
--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -8,7 +8,6 @@ const Navbar = () => {
             className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
             <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
                 <a
-                    href="#about-me"
                     className="h-auto w-auto flex flex-row items-center"
                 >
                     <Image
@@ -20,7 +19,7 @@ const Navbar = () => {
                     />
 
                     <span className="font-bold ml-[10px] hidden md:block text-gray-300">
-                        WebChain Dev
+                        Omurbek
                     </span>
                 </a>
 
