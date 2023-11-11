efe1989c98d0730a7c35766af7f00e4e659e21d5
Omurbek Kurmanbekov <kurmanbekovomurbek80@gmail.com>
2023-11-11 14:32:47 +0600

    fix some bugs

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,14 @@ import Skills from "@/components/main/Skills";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="h-full w-full">
-      <div className="flex flex-col gap-20">
-        <Hero />
-        <Skills />
-        <Encryption />
-        <Projects />
-      </div>
-    </main>
-  );
+    return (
+        <main className="h-full w-full">
+            <div className="flex flex-col gap-20">
+                <Hero/>
+                <Skills/>
+                <Encryption/>
+                <Projects/>
+            </div>
+        </main>
+    );
 }
--- components/sub/SkillDataProvider.tsx
+++ components/sub/SkillDataProvider.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import {motion} from 'framer-motion'
-import { useInView } from 'react-intersection-observer'
+import {useInView} from 'react-intersection-observer'
 import Image from 'next/image';
 
 interface Props {
@@ -12,7 +12,7 @@ interface Props {
     index: number;
 }
 
-const SkillDataProvider = ({ src, width, height, index} : Props) => {
+const SkillDataProvider = ({src, width, height, index}: Props) => {
     const {ref, inView} = useInView({
         triggerOnce: true
     })
@@ -23,23 +23,23 @@ const SkillDataProvider = ({ src, width, height, index} : Props) => {
     }
 
     const animationDelay = 0.3
-  return (
-  <motion.div
-  ref={ref}
-  initial="hidden"
-  variants={imageVariants}
-  animate={inView ? "visible" : "hidden"}
-  custom={index}
-  transition={{delay: index * animationDelay}}
-  >
-    <Image
-src={src}
-width={width}
-height={height}
-alt='skill image'
-    />
-  </motion.div>
-  )
+    return (
+        <motion.div
+            ref={ref}
+            initial="hidden"
+            variants={imageVariants}
+            animate={inView ? "visible" : "hidden"}
+            custom={index}
+            transition={{delay: index * animationDelay}}
+        >
+            <Image
+                src={src}
+                width={width}
+                height={height}
+                alt='skill image'
+            />
+        </motion.div>
+    )
 }
 
 export default SkillDataProvider
